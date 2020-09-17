6eeb788f36aefeb11d13004510f7ba241161d431
Lionel NKEOUA <mbembasachat9@gmail.com>
2020-09-17 11:16:28 +0000

    update App.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react"
+import { useSelector, shallowEqual, useDispatch } from "react-redux"
+import "./index.css"
+import * as actionTypes from "./stores/actions/actionTypes"
+import { Article } from "./components/Article"
+import  AddArticle  from "./components/AddArticle"
+import { addArticle, removeArticle } from "./stores/actions/actionCreators"
+import { Dispatch } from "redux"
 
-function App() {
+const App: React.FC = () => {
+  const articles: readonly actionTypes.IArticle[] = useSelector(
+    (state: actionTypes.ArticleState) => state.articles,
+    shallowEqual
+  )
+
+  const dispatch: Dispatch<any> = useDispatch()
+
+  const saveArticle = React.useCallback(
+    (article: actionTypes.IArticle) => dispatch(addArticle(article)),
+    [dispatch]
+  )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+      <h1>My Articles</h1>
+      <AddArticle saveArticle={saveArticle} />
+      {articles.map((article: actionTypes.IArticle) => (
+        <Article
+          key={article.id}
+          article={article}
+          removeArticle={removeArticle}
+        />
+      ))}
+    </main>
+  )
 }
 
-export default App;
+export default App
